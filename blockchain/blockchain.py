a10525148780794290a44ef558375a56a841f525
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-08-26 16:58:26 -0700

    Blockchain methods

---
+++ blockchain/blockchain.py
@@ -0,0 +1,16 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        #This function creates new blocks and then adds to the existing chain
+        pass
+
+    def new_transaction(self):
+        #This function adds a new transaction to already existing transactions
+        pass
