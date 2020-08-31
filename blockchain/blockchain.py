c13105023cab72977706b8bea61afc8592824c6f
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-08-30 21:10:52 -0700

    [added] newtransaction & newblock

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -9,8 +9,25 @@ class Blockchain(object):
 
     def new_block(self):
         #This function creates new blocks and then adds to the existing chain
-        pass
+        self.new_block(previous_hash=1, proof=100)
 
     def new_transaction(self):
         #This function adds a new transaction to already existing transactions
+        self.current_transactions.append(
+            {
+                'sender':sender,
+                'recipient':recipient,
+                'amount':amount,
+            }
+        )
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        #Used for hashing a block
+        pass
+
+    @property
+    def last_block(self):
+        # Calls and returns the last block of the chain
         pass
