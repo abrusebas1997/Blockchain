7dbd5d89ae3633e2803645d0eb550d3d76583c3a
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-08-30 21:15:52 -0700

    [added] lastblock & hash

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -6,10 +6,22 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
 
     def new_block(self):
         #This function creates new blocks and then adds to the existing chain
-        self.new_block(previous_hash=1, proof=100)
+        block = {
+           'index': len(self.chain) + 1,
+           'timestamp' : time(),
+           'proof': proof,
+           'previous_hash': previous_hash or self.hash(self.chain[-1]),
+       }
+
+   # Set the current transaction list to empty.
+
+   self.current_transactions=[]
+   self.chain.append(block)
+   return block
 
     def new_transaction(self):
         #This function adds a new transaction to already existing transactions
@@ -25,9 +37,10 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         #Used for hashing a block
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # Calls and returns the last block of the chain
-        pass
+        return self.chain[-1]
