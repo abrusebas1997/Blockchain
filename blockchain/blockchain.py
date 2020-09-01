b5d4fae5da2e032f3c7b60f674a2e4e6350850aa
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-08-31 20:14:22 -0700

    [added] proof of proof

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -8,7 +8,19 @@ class Blockchain(object):
         self.current_transactions = []
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof +=1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f’{last_proof}{proof}‘.encode()
+        guess_hash = hashlib.sha256(guess).hexigest()
+        return guess_hash[:4] == “0000”
+
+    def new_block(self, proof, previous_hash=None):
         #This function creates new blocks and then adds to the existing chain
         block = {
            'index': len(self.chain) + 1,
